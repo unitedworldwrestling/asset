149e35cc7b52aeeb276690379beb8c2b7b098835
Emmanuel Veyrat <emmanuel.veyrat@gmail.com>
2018-04-26 20:07:56 +0000

    Capistrano

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,6 +1,6 @@
 set :branch, "master"
 set :deploy_to, "/home/www-data/asset.uww.io"
-set :shared_path, "/home/www-data/efs_share/asset.uww.io/shared"
+set :shared_directory, "/home/www-data/efs_share/asset.uww.io/shared"
 
 server "uww-prod-app-1.nine.ch", user: "www-data", roles: %w{app db web}
 server "uww-prod-app-2.nine.ch", user: "www-data", roles: %w{app db web}
