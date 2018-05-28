8e0c2c2be6a498928e09c5a5641aafb1c9638726
Emmanuel Veyrat <emmanuel.veyrat@gmail.com>
2018-05-28 14:32:17 +0000

    Specify composer bin path

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,8 +1,8 @@
+SSHKit.config.command_map[:composer] = '/home/www-data/bin/composer'
+
 set :branch, "develop"
 set :deploy_to, "/home/www-data/staging.asset.uww.io"
 set :shared_directory, "/home/www-data/efs_share/staging.asset.uww.io/shared"
-
-set :composer_bin, '/home/www-data/bin/composer'
 
 server "uww-staging-app-3.nine.ch", user: "www-data", roles: %w{app db web}
 server "uww-staging-app-4.nine.ch", user: "www-data", roles: %w{app db web}
