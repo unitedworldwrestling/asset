e7268388111ea1b0f9fa956d2e6b3c0805a613de
Emmanuel Veyrat <emmanuel.veyrat@gmail.com>
2018-05-28 14:25:34 +0000

    Specify composer path for deploying on staging-app3 & staging-app4

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,5 +2,7 @@ set :branch, "develop"
 set :deploy_to, "/home/www-data/staging.asset.uww.io"
 set :shared_directory, "/home/www-data/efs_share/staging.asset.uww.io/shared"
 
-server "uww-staging-app-1.nine.ch", user: "www-data", roles: %w{app db web}
-server "uww-staging-app-2.nine.ch", user: "www-data", roles: %w{app db web}
+set :composer_bin, '/home/www-data/bin/composer'
+
+server "uww-staging-app-3.nine.ch", user: "www-data", roles: %w{app db web}
+server "uww-staging-app-4.nine.ch", user: "www-data", roles: %w{app db web}
