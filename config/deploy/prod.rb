f8a40f85ee468a9abb33a460c7e81716c7646576
Emmanuel Veyrat <emmanuel.veyrat@gmail.com>
2018-06-04 09:58:10 +0000

    Adjust capistrano receipe for new servers

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,6 +1,8 @@
+SSHKit.config.command_map[:composer] = '/home/www-data/bin/composer'
+
 set :branch, "master"
 set :deploy_to, "/home/www-data/asset.uww.io"
 set :shared_directory, "/home/www-data/efs_share/asset.uww.io/shared"
 
-server "uww-prod-app-1.nine.ch", user: "www-data", roles: %w{app db web}
-server "uww-prod-app-2.nine.ch", user: "www-data", roles: %w{app db web}
+server "uww-prod-app-3.nine.ch", user: "www-data", roles: %w{app db web}
+server "uww-prod-app-4.nine.ch", user: "www-data", roles: %w{app db web}
